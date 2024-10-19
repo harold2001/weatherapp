07c34fbbecd85137d72be56211f71e98fad0f5ab
Harold Carazas <car23068@byui.edu>
2024-10-19 02:50:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Weather App
+
+## Description
+
+This is a weather app that displays the current weather conditions for a given location. It uses the OpenWeatherMap API to fetch the weather data and displays it in a user-friendly format.
+
+## Features
+
+- Displays the current weather conditions for a given location.
+- Uses the OpenWeatherMap API to fetch the weather data.
+- Displays the weather data in a user-friendly format.
+- Provides a search bar to allow users to enter a location and view the weather conditions.
+- Displays the current temperature, humidity, wind speed, and weather description.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Vite
+- OpenWeatherMap API
---
+++ src/js/models/ExternalServices.mjs
@@ -0,0 +1,36 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+export default class ExternalServices {
+  async getData(category) {
+    const response = await fetch(baseURL + `products/search/${category}`);
+    const data = await this.convertToJson(response);
+    return data.Result;
+  }
+
+  async findProductById(id) {
+    const response = await fetch(baseURL + `product/${id}`);
+    const data = await this.convertToJson(response);
+    return data.Result;
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + "checkout/", options).then(this.convertToJson);
+  }
+
+  async convertToJson(res) {
+    const jsonResponse = await res.json();
+
+    if (res.ok) {
+      return jsonResponse;
+    } else {
+      throw { name: "servicesError", message: jsonResponse };
+    }
+  }
+}
